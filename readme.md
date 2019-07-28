67ca441d814ed17fac6b7931aee3e6eb0fd8bdb2
Matt Steele <c-matt.steele@charter.com>
2019-07-28 16:10:55 -0600

    Update readme

--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
-# Western Code Build
+# Western Code
+
+## What is this project?
+
+An old business website built on Statamic CMS (PHP).
+
+
+## To use:
+
+```shell
+1. Start Valet
+$ valet start
+
+2. Start Gulp
+$ cd westerncode/site/themes/westerncode
+$ gulp
+
+```
+
+## Maintainer
+
+[Matt Steele](https://github.com/mattsteele) - matt@mattsteele@dev
--- site/themes/westerncode/gulpfile.js
+++ site/themes/westerncode/gulpfile.js
@@ -2,31 +2,30 @@
 
 // Run 'npm install'
 
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    cssmin      = require('gulp-cssmin'),
-    rename      = require('gulp-rename'),
-    prefix      = require('gulp-autoprefixer'),
-    uglify      = require('gulp-uglify'),
-    concat      = require('gulp-concat'),
-    imagemin    = require('gulp-imagemin'),
-    beep        = require('beepbeep'),
-    svgmin      = require('gulp-svgmin'),
-    path        = require('path'),
-    browserSync = require('browser-sync').create();
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  cssmin = require('gulp-cssmin'),
+  rename = require('gulp-rename'),
+  prefix = require('gulp-autoprefixer'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  imagemin = require('gulp-imagemin'),
+  svgmin = require('gulp-svgmin'),
+  path = require('path'),
+  browserSync = require('browser-sync').create();
 
 
-gulp.task('serve', ['sass', 'js'], function() {
+gulp.task('serve', ['sass', 'js'], function () {
 
-    browserSync.init({
-            open: 'external',
-            host: 'westerncode.dev',
-            proxy: 'westerncode.dev'
-    });
+  browserSync.init({
+    open: 'external',
+    host: 'westerncode.test',
+    proxy: 'westerncode.test'
+  });
 
-    gulp.watch('src/scss/**/*.scss', ['sass']);
-    gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('./*.html').on('change', browserSync.reload);
+  gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('./*.html').on('change', browserSync.reload);
 });
 
 
@@ -35,27 +34,27 @@ gulp.task('sass', function () {
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(prefix('last 2 versions'))
     .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream())
-    .on('end', function () { beep(); });
+    .on('end', function () { });
 });
 
 
-gulp.task('js', function() {
+gulp.task('js', function () {
   return gulp.src('src/js/**/*.js')
     .pipe(uglify())
     .pipe(concat('app.js'))
-    .pipe(rename({suffix: '.min'}))
+    .pipe(rename({ suffix: '.min' }))
     .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.stream());
 });
 
 
 gulp.task('svgmin', function () {
-    return gulp.src('src/svg/**/*.+(svg)')
-        .pipe(svgmin())
-        .pipe(gulp.dest('dist/svg'));
+  return gulp.src('src/svg/**/*.+(svg)')
+    .pipe(svgmin())
+    .pipe(gulp.dest('dist/svg'));
 });
 
 
